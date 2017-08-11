2250751325be73d6a1b8b7f2307a9784ab705392
vns-123 <30935622+vns-123@users.noreply.github.com>
2017-08-11 10:54:10 -0700

    Updating defualt bot script

--- bot.js
+++ bot.js
@@ -1,3 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
+const token = (token.token);
+const prefix = (config.prefix);
+
+client.login(token.token);
