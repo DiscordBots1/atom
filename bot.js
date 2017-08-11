fb3ad6583982760f8c264e756bde3bd2e94f7ef2
vns-123 <30935622+vns-123@users.noreply.github.com>
2017-08-11 11:02:31 -0700

    Add a better template to default bot script

--- bot.js
+++ bot.js
@@ -5,3 +5,20 @@ const token = (token.token);
 const prefix = (config.prefix);
 
 client.login(token.token);
+
+client.on('ready', () => {
+  console.log(`${client.user.username} is ready!`);
+  client.user.setGame('Still in alpha!');
+});
+
+client.on('message', message => {
+  // Preventing bot-ception
+  if(msg.author.bot) return;
+  if(!msg.content.startsWith(config.prefix)) return;
+  // Defining arguments
+  const args = msg.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+  
+  // Commands will go here \/ \/
+  
+});
